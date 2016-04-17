d2c7ce9ddd5f9216dcb264c3f05d600c2f5d3667
Michael Herndon <mherndon@michaelherndon.com>
2016-04-17 13:52:04 -0400

    WORK updating appveyor configs

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+
+Write-Host "${env:APPVEYOR_BUILD_FOLDER}"
+$items = gci "${env:APPVEYOR_BUILD_FOLDER}\*" -Include *.nupkg -Recurse
+foreach($item in $items) {
+    if($item.FullName.Contains("Tests")) {
+        continue;     
+    } 
+    $pkg = $item.FullName
+    $server = "https://www.myget.org/F/badmishka-dev/api/v2/package";
+    nuget push $pkg -ApiKey ($env:api_key)  -Source $server  
+}
